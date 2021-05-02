d38115cd460ebb88594511322e2430820754bee0
tensaurus <tensaurus@users.noreply.github.com>
2021-05-02 18:40:48 +0530

    followers half way

--- app/models.py
+++ app/models.py
@@ -7,7 +7,10 @@ from app import db, login
 @login.user_loader
 def load_user(id):
     return User.query.get(int(id))
-
+# Association table for follower user and followed user
+followers = db.Table('followers',
+    db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('followed_id', db.Integer, db.ForeignKey('user.id')))
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
@@ -16,6 +19,11 @@ class User(UserMixin, db.Model):
     posts = db.relationship('Post', backref='author', lazy='dynamic')
     about_me = db.Column(db.String(140))
     last_seen = db.Column(db.DateTime, default=datetime.utcnow)
+    followed = db.relationship(
+        'User', secondary=followers,
+        primaryjoin = (followers.c.follower_id == id),
+        secondaryjoin = (followers.c.followed_id == id),
+        backref = db.backref('followers', lazy='dynamic'), lazy='dynamic')
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
@@ -26,6 +34,18 @@ class User(UserMixin, db.Model):
     def avatar(self, size):
         digest = md5(self.email.lower().encode('utf-8')).hexdigest()
         return f'https://gravatar.com/avatar/{digest}?d=identicon&s={size}'
+
+    def follow(self, user):
+        if not is_following(user):
+            self.followed.append(user)
+
+    def unfollow(self, user):
+        if is_following(user):
+            self.followed.remove(user)
+
+    def is_following(self, user):
+        return self.followed.filter(
+            followers.c.follower_id == user.id).count() > 0
 
     def __repr__(self):
         return f'<User {self.username}>'
